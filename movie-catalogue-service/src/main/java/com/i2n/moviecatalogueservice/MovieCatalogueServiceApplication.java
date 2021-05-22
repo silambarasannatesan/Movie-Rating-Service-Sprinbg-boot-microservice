5cfa9e92f612a3bf0e1c2b767117be8f19b7cb4d
Silambarasan N <silambarasan.asn@gmail.com>
2021-05-22 10:19:37 +0530

    First commit - Adding the project structure for the services

---
+++ movie-catalogue-service/src/main/java/com/i2n/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.i2n.moviecatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
+	}
+
+}
